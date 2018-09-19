c8b301b7342dddceec4e4c7e051477abf64bee98
marcuscaum <marcuslucas.jb@gmail.com>
2018-09-19 16:59:39 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './routes/App';
+import './index.css';
+
+const RootApp = <App />;
+ReactDOM.render(RootApp, document.getElementById('root'));
