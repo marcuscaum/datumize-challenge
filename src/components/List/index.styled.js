fda4351e6634b50d2c1ea6e4e9e5a3cf6ebc2687
marcuscaum <marcuslucas.jb@gmail.com>
2018-09-22 03:08:11 -0300

    Update styles for the list

--- src/components/List/index.styled.js
+++ src/components/List/index.styled.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 export default styled.ul`
-  background: white;
   border-radius: 5px;
   box-sizing: border-box;
   padding: 0;
+  margin: 25px 0;
 
   li:last-child {
     border-bottom: none;
--- src/components/ListItem/index.styled.js
+++ src/components/ListItem/index.styled.js
@@ -2,8 +2,13 @@ import styled from 'styled-components';
 
 export default styled.li`
   list-style-type: none;
+  background-color: white;
   padding: 15px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  box-shadow: 3px 3px 3px 1px rgba(0, 0, 0, 0.03);
   border-bottom: 1px solid #e6ebed;
+
   span {
     font-size: 20px;
     margin-bottom: 10px;
