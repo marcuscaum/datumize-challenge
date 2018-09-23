919d7267fc34603887b69657e528f9aae860483f
marcuscaum <marcuslucas.jb@gmail.com>
2018-09-22 23:16:44 -0300

    Create a rolesList component

---
+++ src/components/RolesList/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withHandlers } from 'recompose';
+
+import { RolesList, RoleItem, RoleButton } from './index.styled';
+
+const RolesListComponent = ({
+  roles, projectTeam, projectId, onClickRoleButton,
+}) => (
+  <RolesList>
+    {roles.map((role) => {
+      const member = projectTeam && projectTeam.find(item => item.role === role.name);
+      const memberName = member && member.name;
+
+      return (
+        <RoleItem key={role.id} hasMember={memberName}>
+          <strong>{role.name}</strong>
+          <RoleButton
+            onClick={() => {
+              onClickRoleButton({
+                projectId,
+                role: role.name,
+                user: memberName,
+              });
+            }}
+          >
+            {memberName || 'Unassigned'}
+          </RoleButton>
+        </RoleItem>
+      );
+    })}
+  </RolesList>
+);
+
+RolesListComponent.propTypes = {
+  roles: PropTypes.instanceOf(Array).isRequired,
+  projectTeam: PropTypes.string.isRequired,
+  projectId: PropTypes.number.isRequired,
+  onClickRoleButton: PropTypes.func.isRequired,
+};
+
+export default withHandlers({
+  onClickRoleButton: ({ roleValuesHandler, openPortal }) => ({ projectId, role, user }) => {
+    roleValuesHandler({
+      projectId,
+      role,
+      user,
+    });
+    openPortal();
+  },
+})(RolesListComponent);
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -5,20 +5,14 @@ import { compose, withState } from 'recompose';
 import List from '../../components/List';
 import ListItem from '../../components/ListItem';
 import UsersList from '../../components/UsersListModal';
+import RolesList from '../../components/RolesList';
 
 import withModal from '../../hocs/with-modal';
 
 import AddItemForm from '../../components/AddItemForm';
 
-import { RolesList, RoleItem, RoleButton } from './index.styled';
-
 const ProjectsPage = ({
-  projects,
-  roles,
-  createProject,
-  deleteProject,
-  openPortal,
-  roleValuesHandler,
+  projects, roles, createProject, deleteProject, ...rest
 }) => (
   <React.Fragment>
     <AddItemForm
@@ -36,30 +30,7 @@ const ProjectsPage = ({
         data={projects}
         renderItem={data => (
           <ListItem key={data.id} data={data.name}>
-            <RolesList>
-              {roles.map((role) => {
-                const member = data.team && data.team.find(item => item.role === role.name);
-                const memberName = member && member.name;
-
-                return (
-                  <RoleItem key={role.id} hasMember={memberName}>
-                    <strong>{role.name}</strong>
-                    <RoleButton
-                      onClick={() => {
-                        roleValuesHandler({
-                          projectId: data.id,
-                          role: role.name,
-                          user: memberName,
-                        });
-                        openPortal();
-                      }}
-                    >
-                      {memberName || 'Unassigned'}
-                    </RoleButton>
-                  </RoleItem>
-                );
-              })}
-            </RolesList>
+            <RolesList roles={roles} projectId={data.id} projectTeam={data.team} {...rest} />
           </ListItem>
         )}
       />
@@ -78,8 +49,6 @@ ProjectsPage.propTypes = {
   roles: PropTypes.instanceOf(Array).isRequired,
   createProject: PropTypes.func.isRequired,
   deleteProject: PropTypes.func.isRequired,
-  roleValuesHandler: PropTypes.func.isRequired,
-  openPortal: PropTypes.func.isRequired,
 };
 
 export default compose(
